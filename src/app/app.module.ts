fb1c6dd9b78d9c3559afde9083e87124d3c36416
denis.kalenga <engineereldorado@gmail.com>
2018-08-03 18:40:27 +0300

    wysiwyg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { LoginComponent } from './login/login.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { SendMailComponent } from './send-mail/send-mail.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AngularEditorModule } from '@kolkov/angular-editor';
 const appRoutes: Routes = [
   
   {path:'', component:LoginComponent},
@@ -30,8 +33,12 @@ const appRoutes: Routes = [
     NgbModule.forRoot(),
     BrowserModule,
     HttpClientModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot(),
     NgxSpinnerModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFontAwesomeModule,
+    AngularEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/send-mail/send-mail.component.ts
+++ src/app/send-mail/send-mail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UsersService } from '../users.service';
 import { Email } from '../send-mail/Email';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 @Component({
   selector: 'app-send-mail',
   templateUrl: './send-mail.component.html',
@@ -11,6 +12,30 @@ export class SendMailComponent implements OnInit {
 
   myForm:FormGroup;
   wrong:boolean;
+
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    customClasses: [
+      {
+        name: "quote",
+        class: "quote",
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: "titleText",
+        class: "titleText",
+        tag: "h1",
+      },
+    ]
+  };
   constructor(private userService:UsersService) { }
 
   ngOnInit() {
