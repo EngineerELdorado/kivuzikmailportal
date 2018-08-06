c23ce53742f5f33177bd9c16a6cdc6724d7f3ded
denis.kalenga <engineereldorado@gmail.com>
2018-08-06 16:51:41 +0300

    thread bug fix

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -8,7 +8,7 @@ import { Email } from './send-mail/Email';
 })
 export class UsersService {
 
-  //URL = "http://192.168.100.2:5050";
+  //URL = "http://192.168.0.141:5050";
   URL="https://kizuzikmail.herokuapp.com";
 
   constructor(private http:HttpClient) { }
